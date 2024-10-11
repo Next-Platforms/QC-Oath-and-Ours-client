45f1d5338647d4f66f1f0f00f477262bc3baf24b
Kshitij Pawar <pawarkshitij888@gmail.com>
2024-10-11 17:59:59 +0530

    create features page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      colors: {
-        peach: "hsl(var(--peach))",
-        lime: "hsl(var(--lime))",
-      },
-    },
-  },
-  plugins: [],
-};
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				peach: 'hsl(var(--peach))',
+				lime: 'hsl(var(--lime))'
+			}
+		}
+	},
+	plugins: [
+		function ({ addUtilities }) {
+			const newUtilities = {
+				'.page-container': {
+					width: '100%',
+					'@screen sm': {
+						'max-width': '640px'
+					},
+					'@screen md': {
+						'max-width': '768px'
+					},
+					'@screen lg': {
+						'max-width': '1024px'
+					},
+					'@screen xl': {
+						'max-width': '1200px'
+					}
+				}
+			}
+
+			addUtilities(newUtilities, ['responsive'])
+		}
+	]
+}
