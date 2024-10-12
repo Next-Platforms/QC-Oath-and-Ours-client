c1a8e18cc1920b09ecd5588ededd70b4e0b2ac82
Kshitij Pawar <pawarkshitij888@gmail.com>
2024-10-12 15:27:55 +0530

    add content to mobile drawer, add social links to footer and update some assets

--- src/components/navbar-drawer.tsx
+++ src/components/navbar-drawer.tsx
@@ -1,4 +1,23 @@
 import { Drawer, DrawerContent, DrawerTrigger } from './ui/drawer'
+import Logo from '../icons/logo.svg'
+
+const links = [
+	{
+		title: 'スタジオ紹介',
+		subtitle: 'STUDIO',
+		href: '/features'
+	},
+	{
+		title: 'ギャラリー',
+		subtitle: 'GALLERY',
+		href: '/gallery'
+	},
+	{
+		title: 'コラム',
+		subtitle: 'COLUMN',
+		href: '/column'
+	}
+]
 
 const NavbarDrawer = () => {
 	return (
@@ -8,10 +27,29 @@ const NavbarDrawer = () => {
 					<img className="h-6 w-10" src="/hamburger.svg" />
 				</button>
 			</DrawerTrigger>
-			<DrawerContent
-				className="h-full w-full max-w-[375px] rounded-none"
-				includeHandle={false}
-			></DrawerContent>
+			<DrawerContent className="h-full w-full max-w-[375px] rounded-none" includeHandle={false}>
+				<div className="flex h-full flex-col items-center justify-center gap-[44px] px-6">
+					<img className="h-[45px] w-[192px]" src={Logo.src} />
+					{links.map((link) => {
+						return (
+							<a
+								className="flex flex-col items-center text-center"
+								href={link.href}
+								key={link.title}
+							>
+								<span className="text-base font-bold leading-[19px]">{link.title}</span>
+								<span className="text-base leading-[19px] text-[#D7D7D7]">{link.subtitle}</span>
+							</a>
+						)
+					})}
+					<div className="flex h-[80px] w-full items-center justify-center bg-lime px-6 text-center text-white">
+						<p className="text-base font-bold leading-[19px]">
+							<span className="text-[18px] leading-[21px]">LINE</span>で<br />
+							問い合わせる
+						</p>
+					</div>
+				</div>
+			</DrawerContent>
 		</Drawer>
 	)
 }
--- src/components/ui/pagination-controls.tsx
+++ src/components/ui/pagination-controls.tsx
@@ -1,5 +1,29 @@
-import { useMemo } from 'react'
+import React, { useMemo } from 'react'
 import { cn } from '../../lib/utils'
+
+const PaginationControlButton = ({
+	children,
+	className,
+	active = false,
+	type = 'button',
+	...rest
+}: React.ComponentPropsWithoutRef<'button'> & {
+	active?: boolean
+}) => {
+	return (
+		<button
+			type={type}
+			className={cn(
+				'inline-flex h-[35px] w-[35px] items-center justify-center rounded-full border-2 border-black text-black hover:bg-[#333333] hover:text-white focus-visible:bg-[#333333] focus-visible:text-white',
+				active && 'bg-black text-white',
+				className
+			)}
+			{...rest}
+		>
+			{children}
+		</button>
+	)
+}
 
 export const PaginationControls = ({
 	page,
@@ -20,36 +44,55 @@ export const PaginationControls = ({
 
 	return (
 		<div className="flex items-center gap-[11px]">
-			<button
-				className="inline-flex h-[35px] w-[35px] items-center justify-center rounded-full border-2 border-black"
+			<PaginationControlButton
+				className="group"
 				disabled={page === 1}
 				onClick={() => setPage(page - 1)}
 			>
-				<img className={'h-[10px] w-[5px]'} src="./left-arrow.svg" />
-			</button>
+				<svg
+					className={
+						'h-[10px] w-[5px] stroke-[#333] group-hover:stroke-white group-focus-visible:stroke-white'
+					}
+					id="Layer_2"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 6.06 10.71"
+					fill="none"
+				>
+					<g id="content">
+						<path d="M5.71,10.35L.71,5.35,5.71.35" />
+					</g>
+				</svg>
+			</PaginationControlButton>
 			{new Array(Math.min(5, totalPages)).fill(0).map((_, idx) => {
 				const pageNumber = idx + page
 				const isActive = pageNumber === page
 
 				return (
-					<button
-						className={cn(
-							'inline-flex h-[35px] w-[35px] items-center justify-center rounded-full border-2 border-black',
-							isActive && 'bg-black text-white'
-						)}
-						onClick={() => setPage(pageNumber)}
-					>
+					<PaginationControlButton active={isActive} onClick={() => setPage(pageNumber)}>
 						{pageNumber}
-					</button>
+					</PaginationControlButton>
 				)
 			})}
-			<button
-				className="inline-flex h-[35px] w-[35px] items-center justify-center rounded-full border-2 border-black"
+
+			<PaginationControlButton
+				className="group"
 				disabled={page >= totalPages}
 				onClick={() => setPage(page + 1)}
 			>
-				<img className={'h-[10px] w-[5px] rotate-180'} src="./left-arrow.svg" />
-			</button>
+				<svg
+					className={
+						'h-[10px] w-[5px] rotate-180 stroke-[#333] group-hover:stroke-white group-focus-visible:stroke-white'
+					}
+					id="Layer_2"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 6.06 10.71"
+					fill="none"
+				>
+					<g id="content">
+						<path d="M5.71,10.35L.71,5.35,5.71.35" />
+					</g>
+				</svg>
+			</PaginationControlButton>
 		</div>
 	)
 }
