81d54165f381fbe4fe0bd4e7c85835f43e26f6d0
Kshitij Pawar <pawarkshitij888@gmail.com>
2024-10-21 16:14:29 +0530

    adjust gallerys select styles

--- src/components/page-components/gallery/gallery-tabs.tsx
+++ src/components/page-components/gallery/gallery-tabs.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import * as SelectPrimitive from '@radix-ui/react-select'
-import ChevronDownIcon from '../../../../public/hamburger.svg'
+import ChevronDownIcon from '../../../../public/chevron-down.svg'
 import { cn, updateSearchParams } from '../../../lib/utils'
 
 export const GalleryTabs = ({
@@ -66,7 +66,7 @@ const SelectItem = React.forwardRef<
 	<SelectPrimitive.Item
 		ref={ref}
 		className={cn(
-			'relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-2 pr-8 text-center text-[18px] font-bold leading-[21px] text-black outline-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
+			'relative flex w-full cursor-default select-none items-center rounded-sm py-[10px] pl-2 text-center text-[18px] font-bold leading-[21px] text-black outline-none first:pt-0 data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
 			className
 		)}
 		{...props}
@@ -92,7 +92,7 @@ const SelectTrigger = React.forwardRef<
 	>
 		{children}
 		<SelectPrimitive.Icon asChild>
-			<img className="ml-2 h-4 w-4 object-cover opacity-50" src={ChevronDownIcon.src} />
+			<img className="ml-2 h-[3.7px] w-[13.5px] object-cover" src={ChevronDownIcon.src} />
 		</SelectPrimitive.Icon>
 	</SelectPrimitive.Trigger>
 ))
@@ -106,7 +106,7 @@ const SelectContent = React.forwardRef<
 		<SelectPrimitive.Content
 			ref={ref}
 			className={cn(
-				'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-b-md border border-[#999999] bg-white shadow-md',
+				'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-b-md border border-t-0 border-[#999999] bg-white shadow-md',
 				position === 'popper' &&
 					'data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1',
 				className
